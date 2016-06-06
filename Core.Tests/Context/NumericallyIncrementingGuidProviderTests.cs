42bdff3d9313f0914e0d9c001ab5a9697da7350c
AnthonyDewhirst <anthony.dewhirst@gmail.com>
2016-06-06 15:23:25 +0100

    Added missing NumericallyIncrementingGuidProvider tests

---
+++ Core.Tests/Context/NumericallyIncrementingGuidProviderTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sfa.Core.Testing;
+
+namespace Sfa.Core.Context
+{
+    [TestClass]
+    public class NumericallyIncrementingGuidProviderTests : BaseTest
+    {
+        #region NewGuid
+
+        [TestMethod, TestCategory("Unit")]
+        public void NewGuid_DefaultSeed()
+        {
+            // Arrange
+            var componentUnderTest = new NumericallyIncrementingGuidProvider();
+
+            // Act / Assert
+            componentUnderTest.NewGuid().ShouldHaveSameValueAs(Guid.Parse("00000000-0000-0000-0000-000000000000"));
+            componentUnderTest.NewGuid().ShouldHaveSameValueAs(Guid.Parse("00000000-0000-0000-0000-000000000001"));
+            componentUnderTest.NewGuid().ShouldHaveSameValueAs(Guid.Parse("00000000-0000-0000-0000-000000000002"));
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void NewGuid_UsingSeed()
+        {
+            // Arrange
+            var componentUnderTest = new NumericallyIncrementingGuidProvider();
+
+            // Act
+            componentUnderTest.SetSeedValue(1000);
+
+            // Assert
+            componentUnderTest.NewGuid().ShouldHaveSameValueAs(Guid.Parse("00000000-0000-0000-0000-000000001000"));
+            componentUnderTest.NewGuid().ShouldHaveSameValueAs(Guid.Parse("00000000-0000-0000-0000-000000001001"));
+            componentUnderTest.NewGuid().ShouldHaveSameValueAs(Guid.Parse("00000000-0000-0000-0000-000000001002"));
+        }
+
+        #endregion
+    }
+}
