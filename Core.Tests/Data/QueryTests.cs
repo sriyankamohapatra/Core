ad016421a6e8c179eb248c998b0d0d5ab9e11205
AnthonyDewhirst <anthony.dewhirst@gmail.com>
2016-06-07 09:56:29 +0100

    Added missing Query Tests

--- Core.Tests/Data/QueryTests.cs
+++ Core.Tests/Data/QueryTests.cs
@@ -1,5 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sfa.Core.Exceptions;
 using Sfa.Core.Testing;
 
 namespace Sfa.Core.Data
@@ -7,6 +11,16 @@ namespace Sfa.Core.Data
     [TestClass]
     public class QueryTests : BaseTest
     {
+        #region Life Cycle
+
+        protected override IEnumerable<Assembly> AssembliesWithTypesToPerformFieldValueEqualityOn
+        {
+            get { yield return typeof(QueryTests).Assembly; }
+        }
+
+        #endregion
+
+
         #region Test Classes
 
         public class DefaultQuery : Query<string, string>
@@ -36,6 +50,75 @@ namespace Sfa.Core.Data
             }
         }
 
+        public class SimplePocoQuery<TReturn> : Query<TReturn, SimplePoco>
+        {
+            protected override IQueryable<SimplePoco> Queryable => new[]
+            {
+                new SimplePoco
+                {
+                    Id = 1,
+                    Value1 = "v1.1",
+                    Value2 = "v1.2",
+                    ChildPoco = new SimplePoco
+                    {
+                        Id = 2,
+                        Value1 = "v2.1"
+                    }
+                },
+                new SimplePoco
+                {
+                    Id = 3,
+                    Value1 = "v3.1",
+                    Value2 = "v3.2",
+                    ChildPoco = new SimplePoco
+                    {
+                        Id = 4,
+                        Value1 = "v4.1"
+                    }
+                },
+            }.AsQueryable();
+
+            protected override IQueryable<SimplePoco> FormLinqQuery(IQueryable<SimplePoco> query)
+            {
+                return query;
+            }
+        }
+
+        public class SimplePoco
+        {
+            public int Id { get; set; }
+
+            public string Value1 { get; set; }
+
+            public string Value2 { get; set; }
+
+            public SimplePoco ChildPoco { get; set; }
+        }
+
+        public class MissingImplementationProjection : IProjection
+        {
+            public string Value1 { get; set; }
+
+            public string ChildValue1 { get; set; }
+        }
+
+        public class SimplePocoProjection : IProjection
+        {
+            public string Value1 { get; set; }
+
+            public string ChildValue1 { get; set; }
+
+            public static IQueryable<SimplePocoProjection> AddProjection(IQueryable<SimplePoco> query)
+            {
+                return from simplePoco in query
+                    select new SimplePocoProjection
+                    {
+                        Value1 = simplePoco.Value1,
+                        ChildValue1 = simplePoco.ChildPoco.Value1
+                    };
+            }
+        }
+
         #endregion
 
 
@@ -54,6 +137,8 @@ namespace Sfa.Core.Data
 
             // Assert
             actual.ShouldHaveSameValueAs(expected);
+            componentUnderTest.PageNumber.ShouldHaveSameValueAs(0);
+            componentUnderTest.PageSize.ShouldHaveSameValueAs(2);
         }
 
 
@@ -69,6 +154,8 @@ namespace Sfa.Core.Data
 
             // Assert
             actual.ShouldHaveSameValueAs(expected);
+            componentUnderTest.PageNumber.ShouldHaveSameValueAs(1);
+            componentUnderTest.PageSize.ShouldHaveSameValueAs(2);
         }
 
         #endregion
@@ -138,6 +225,145 @@ namespace Sfa.Core.Data
 
             // Assert
             actual.ShouldHaveSameValueAs(false);
+        }
+
+        #endregion
+
+
+        #region GetSingle
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetSingle_Exists()
+        {
+            // Arrange
+            var componentUnderTest = new MatchQuery("two");
+
+            // Act
+            var actual = componentUnderTest.GetSingle();
+
+            // Assert
+            actual.ShouldHaveSameValueAs("two");
+        }
+
+
+        [TestMethod, TestCategory("Unit")]
+        [ExpectedException(typeof(MissingEntityException))]
+        public void GetSingle_DoesNotExists()
+        {
+            // Arrange
+            var componentUnderTest = new MatchQuery("four");
+
+            // Act
+            componentUnderTest.GetSingle();
+        }
+
+        #endregion
+
+
+        #region GetSingleIfExists
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetSingleIfExists_Exists()
+        {
+            // Arrange
+            var componentUnderTest = new MatchQuery("two");
+
+            // Act
+            var actual = componentUnderTest.GetSingleIfExists();
+
+            // Assert
+            actual.ShouldHaveSameValueAs("two");
+        }
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetSingleIfExists_DoesNotExists()
+        {
+            // Arrange
+            var componentUnderTest = new MatchQuery("four");
+
+            // Act
+            var actual = componentUnderTest.GetSingleIfExists();
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        #endregion
+
+
+        #region GetAnyItemsExist
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetAnyItemsExist_Exists()
+        {
+            // Arrange
+            var componentUnderTest = new MatchQuery("two");
+
+            // Act
+            var actual = componentUnderTest.GetAnyItemsExist();
+
+            // Assert
+            actual.ShouldHaveSameValueAs(true);
+        }
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetAnyItemsExist_DoesNotExists()
+        {
+            // Arrange
+            var componentUnderTest = new MatchQuery("four");
+
+            // Act
+            var actual = componentUnderTest.GetAnyItemsExist();
+
+            // Assert
+            actual.ShouldHaveSameValueAs(false);
+        }
+
+        #endregion
+
+
+        #region Projections
+        
+        [TestMethod, TestCategory("Unit")]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void Projections_MissingImplementation()
+        {
+            // Arrange
+            var componentUnderTest = new SimplePocoQuery<MissingImplementationProjection>();
+
+            // Act
+            componentUnderTest.GetList();
+        }
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void Projections()
+        {
+            // Arrange
+            var componentUnderTest = new SimplePocoQuery<SimplePocoProjection>();
+
+            // Act
+            var actual = componentUnderTest.GetList();
+
+            // Assert
+            actual.ShouldHaveSameValueAs(new []
+            {
+                new SimplePocoProjection
+                {
+                    Value1 = "v1.1",
+                    ChildValue1 = "v2.1"
+                },
+                new SimplePocoProjection
+                {
+                    Value1 = "v3.1",
+                    ChildValue1 = "v4.1"
+                },
+            });
         }
 
         #endregion
