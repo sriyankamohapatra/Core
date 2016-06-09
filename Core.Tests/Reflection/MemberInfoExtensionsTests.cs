e4b48395a863316faef08df1ddd14077f91f2578
AnthonyDewhirst <anthony.dewhirst@gmail.com>
2016-06-09 12:48:42 +0100

    Added missing MemberInfoExtensions tests

---
+++ Core.Tests/Reflection/MemberInfoExtensionsTests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sfa.Core.Testing;
+
+namespace Sfa.Core.Reflection
+{
+    [TestClass]
+    public class MemberInfoExtensionsTests : BaseTest
+    {
+        #region Test Classes
+
+        public class SimpleAttribute : Attribute
+        {
+            
+        }
+
+
+        public class ChildSimpleAttribute : SimpleAttribute
+        {
+
+        }
+
+        public class SimplePoco
+        {
+            [Simple]
+            public string MyString { get; set; }
+
+            [ChildSimple]
+            public string MyOtherString { get; set; }
+
+            [ChildSimple] public string MyOtherStringField;
+
+            public int MyInt { get; set; }
+
+            [ChildSimple]
+            public virtual void MyMethod()
+            {
+            }
+        }
+
+        public class ChildPoco : SimplePoco
+        {
+            public override void MyMethod()
+            {
+            }
+        }
+
+        #endregion
+
+
+        #region IsDefined
+
+        [TestMethod, TestCategory("Unit")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IsDefined_MemberInfoNull()
+        {
+            // Act
+            ((MemberInfo) null).IsDefined<Attribute>();
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void IsDefined_True()
+        {
+            // Arrange
+            var componentUnderTest = typeof(SimplePoco).GetProperty(nameof(SimplePoco.MyString));
+
+            // Act
+            var actual = componentUnderTest.IsDefined<SimpleAttribute>();
+
+            // Assert
+            actual.ShouldHaveSameValueAs(true);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void IsDefined_True_InheritedSetTrue_Property()
+        {
+            // Arrange
+            var componentUnderTest = typeof(SimplePoco).GetProperty(nameof(SimplePoco.MyOtherString));
+
+            // Act
+            var actual = componentUnderTest.IsDefined<SimpleAttribute>();
+
+            // Assert
+            actual.ShouldHaveSameValueAs(true);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void IsDefined_True_InheritedSetFalse_Property()
+        {
+            // Arrange
+            var componentUnderTest = typeof(SimplePoco).GetProperty(nameof(SimplePoco.MyOtherString));
+            
+            // Act
+            var actual = componentUnderTest.IsDefined<SimpleAttribute>(false);
+
+            // Assert
+            actual.ShouldHaveSameValueAs(true);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void IsDefined_True_InheritedSetTrue_Field()
+        {
+            // Arrange
+            var componentUnderTest = typeof(SimplePoco).GetField(nameof(SimplePoco.MyOtherStringField));
+
+            // Act
+            var actual = componentUnderTest.IsDefined<SimpleAttribute>();
+
+            // Assert
+            actual.ShouldHaveSameValueAs(true);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void IsDefined_True_InheritedSetFalse_Field()
+        {
+            // Arrange
+            var componentUnderTest = typeof(SimplePoco).GetField(nameof(SimplePoco.MyOtherStringField));
+
+            // Act
+            var actual = componentUnderTest.IsDefined<SimpleAttribute>(false);
+
+            // Assert
+            actual.ShouldHaveSameValueAs(true);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void IsDefined_True_InheritedSetTrue_Method()
+        {
+            // Arrange
+            var componentUnderTest = typeof(ChildPoco).GetMethod(nameof(ChildPoco.MyMethod));
+
+            // Act
+            var actual = componentUnderTest.IsDefined<SimpleAttribute>();
+
+            // Assert
+            actual.ShouldHaveSameValueAs(true);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void IsDefined_False_InheritedSetFalse_Method()
+        {
+            // Arrange
+            var componentUnderTest = typeof(ChildPoco).GetMethod(nameof(ChildPoco.MyMethod));
+
+            // Act
+            var actual = componentUnderTest.IsDefined<SimpleAttribute>(false);
+
+            // Assert
+            actual.ShouldHaveSameValueAs(false);
+        }
+
+        #endregion
+    }
+}
--- Core/Reflection/MemberInfoExtensions.cs
+++ Core/Reflection/MemberInfoExtensions.cs
@@ -9,15 +9,21 @@ namespace Sfa.Core.Reflection
     public static class MemberInfoExtensions
     {
         /// <summary>
-        /// Returns a flag indicating is a custom attribute exists on the target.
+        /// Returns a flag indicating if a custom attribute exists on the target <see cref="MemberInfo"/>.
         /// </summary>
         /// <typeparam name="T">The type of the attribute that is being looked for</typeparam>
         /// <param name="memberInfo">The target of the search.</param>
         /// <param name="inherit">true to search this member's inheritance chain to find the attributes; 
         /// otherwise, false. This parameter is ignored for properties and events; see Remarks.</param>
         /// <returns><c>true</c> if the attribute exists; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="memberInfo"/> is <c>null</c>.</exception>
         public static bool IsDefined<T>(this MemberInfo memberInfo, bool inherit = true) where T : Attribute
         {
+            if (memberInfo == null)
+            {
+                throw new ArgumentNullException(nameof(memberInfo));
+            }
+
             return memberInfo.IsDefined(typeof(T), inherit);
         }
     }
