e805e12839bf2b7da2747fbdd348721764b3f64e
AnthonyDewhirst <anthony.dewhirst@gmail.com>
2016-06-09 15:32:42 +0100

    Added missing EnumTranslator tests

---
+++ Core.Tests/EnumTranslatorTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sfa.Core.Testing;
+
+namespace Sfa.Core
+{
+    [TestClass]
+    public class EnumTranslatorTests : BaseTest
+    {
+        #region Test Classes
+
+        public enum TestEnum
+        {
+            Value0 = 0,
+            Value1 = 1,
+            Value2 = 2,
+            Value4 = 4
+        }
+
+        #endregion
+
+
+        #region Translate
+
+        [TestMethod, TestCategory("Unit")]
+        public void Translate_Matches()
+        {
+            // Arrange
+            var expected = TestEnum.Value0;
+
+            // Act
+            var actual = EnumTranslator.Translate(0, TestEnum.Value1);
+
+            // Assert
+            actual.ShouldHaveSameValueAs(expected);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void Translate_Null_UseDefault()
+        {
+            // Arrange
+            var expected = TestEnum.Value1;
+
+            // Act
+            var actual = EnumTranslator.Translate(null, TestEnum.Value1);
+
+            // Assert
+            actual.ShouldHaveSameValueAs(expected);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void Translate_NotTranslated_UseDefault()
+        {
+            // Arrange
+            var expected = TestEnum.Value1;
+
+            // Act
+            var actual = EnumTranslator.Translate(99, TestEnum.Value1);
+
+            // Assert
+            actual.ShouldHaveSameValueAs(expected);
+        }
+
+        #endregion
+    }
+}
--- Core/EnumTranslator.cs
+++ Core/EnumTranslator.cs
@@ -12,12 +12,13 @@ namespace Sfa.Core
         /// the parsing of the int fails.
         /// </summary>
         /// <typeparam name="T">The type of the enum to translate the value into.</typeparam>
-        /// <param name="value">The value to transalte.</param>
+        /// <param name="value">The value to translate.</param>
         /// <param name="default">The value to use if the parsing of the int fails.</param>
-        /// <returns>The transalted enum.</returns>
+        /// <returns>The translated enum.</returns>
         public static T Translate<T>(int? value, T @default)
+            where T : struct // Would love to make this Enum but the complier currently doesn't allow this.
         {
-            if (!value.HasValue || !Enum.IsDefined(typeof(T), value))
+            if (!value.HasValue || !Enum.IsDefined(typeof(T), value.Value))
             {
                 return @default;
             }
