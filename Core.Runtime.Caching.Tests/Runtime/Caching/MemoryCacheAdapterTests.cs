fbccdeced02f8ee68ea7870908869d736c5d8e89
AnthonyDewhirst <anthony.dewhirst@gmail.com>
2016-07-20 23:07:48 +0100

    Added in caching

---
+++ Core.Runtime.Caching.Tests/Runtime/Caching/MemoryCacheAdapterTests.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Runtime.Caching;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sfa.Core.Context;
+using Sfa.Core.Testing;
+
+namespace Sfa.Core.Runtime.Caching
+{
+    [TestClass]
+    public class MemoryCacheAdapterTests : BaseTest
+    {
+        #region ContainsKey
+
+        [TestMethod, TestCategory("Unit")]
+        public void ContainsKey_True()
+        {
+            // Arrange
+            MemoryCache.Default.Add("ContainsKey_True", "test value", new CacheItemPolicy());
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.ContainsKey("ContainsKey_True");
+
+            // Assert
+            actual.ShouldHaveSameValueAs(true);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void ContainsKey_False()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("ContainsKey_False");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.ContainsKey("ContainsKey_False");
+
+            // Assert
+            actual.ShouldHaveSameValueAs(false);
+        }
+
+        #endregion
+
+
+        #region Delete
+
+        [TestMethod, TestCategory("Unit")]
+        public void Delete_Exists()
+        {
+            // Arrange
+            MemoryCache.Default.Add("Delete_Exists", "some value", new CacheItemPolicy());
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.Delete("Delete_Exists");
+
+            // Assert
+            MemoryCache.Default.Contains("Delete_Exists").ShouldHaveSameValueAs(false);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void Delete_NotExists()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("Delete_NotExists");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.Delete("Delete_NotExists");
+
+            // Assert
+            MemoryCache.Default.Contains("Delete_NotExists").ShouldHaveSameValueAs(false);
+        }
+
+        #endregion
+
+
+        #region Get
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void Get_Exists()
+        {
+            // Arrange
+            MemoryCache.Default.Add("Get_Exists", "some value", new CacheItemPolicy());
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.Get<string>("Get_Exists");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("some value");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void Get_NotExists()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("Get_NotExists");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.Get<string>("Get_NotExists");
+
+            // Assert
+            actual.ShouldHaveSameValueAs(null);
+        }
+
+        #endregion
+
+
+        #region GetOrAddAndGet
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetOrAddAndGet_Exists()
+        {
+            // Arrange
+            MemoryCache.Default.Add("GetOrAddAndGet_Exists", "value1", new CacheItemPolicy());
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.GetOrAddAndGet("GetOrAddAndGet_Exists", () => "value2");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("value1");
+            MemoryCache.Default.Get("GetOrAddAndGet_Exists").ShouldHaveSameValueAs("value1");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetOrAddAndGet_NotExists()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("GetOrAddAndGet_NotExists");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.GetOrAddAndGet("GetOrAddAndGet_NotExists", () => "value2");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("value2");
+            MemoryCache.Default.Get("GetOrAddAndGet_NotExists").ShouldHaveSameValueAs("value2");
+        }
+
+
+        #endregion
+
+
+        #region GetOrAddAndGetSlidingExpiration
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetOrAddAndGetSlidingExpiration_Exists()
+        {
+            // Arrange
+            MemoryCache.Default.Add("GetOrAddAndGetSlidingExpiration_Exists", "value1", new CacheItemPolicy());
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.GetOrAddAndGetSlidingExpiration("GetOrAddAndGetSlidingExpiration_Exists", TimeSpan.FromMinutes(1), () => "value2");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("value1");
+            MemoryCache.Default.Get("GetOrAddAndGetSlidingExpiration_Exists").ShouldHaveSameValueAs("value1");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetOrAddAndGetSlidingExpiration_NotExists()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("GetOrAddAndGetSlidingExpiration_NotExists");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.GetOrAddAndGetSlidingExpiration("GetOrAddAndGetSlidingExpiration_NotExists", TimeSpan.FromMinutes(1), () => "value2");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("value2");
+            MemoryCache.Default.Get("GetOrAddAndGetSlidingExpiration_NotExists").ShouldHaveSameValueAs("value2");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetOrAddAndGetSlidingExpiration_NotExists_RenewAndThenTimeout()
+        {
+            // Arrange
+            const string key = "GetOrAddAndGetSlidingExpiration_NotExists_RenewAndThenTimeout";
+            MemoryCache.Default.Remove(key);
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.GetOrAddAndGetSlidingExpiration(key, TimeSpan.FromSeconds(2), () => "value2");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("value2");
+            MemoryCache.Default.Get(key).ShouldHaveSameValueAs("value2");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Thread.Sleep(1000);
+                componentUnderTest.Get<string>(key).ShouldHaveSameValueAs("value2");
+            }
+            Thread.Sleep(2100);
+            componentUnderTest.Get<string>(key).ShouldHaveSameValueAs(null);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetOrAddAndGetSlidingExpiration_NotExists_Timeout()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("GetOrAddAndGetSlidingExpiration_NotExists_Timeout");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.GetOrAddAndGetSlidingExpiration("GetOrAddAndGetSlidingExpiration_NotExists_Timeout", TimeSpan.FromSeconds(2), () => "value2");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("value2");
+            MemoryCache.Default.Get("GetOrAddAndGetSlidingExpiration_NotExists_Timeout").ShouldHaveSameValueAs("value2");
+
+            Thread.Sleep(2100);
+            componentUnderTest.Get<string>("GetOrAddAndGetSlidingExpiration_NotExists_Timeout").ShouldHaveSameValueAs(null);
+        }
+
+
+        #endregion
+
+
+        #region GetOrAddAndGetExactExpiration
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetOrAddAndGetExactExpiration_Exists()
+        {
+            // Arrange
+            MemoryCache.Default.Add("GetOrAddAndGetExactExpiration_Exists", "value1", new CacheItemPolicy());
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.GetOrAddAndGetExactExpiration("GetOrAddAndGetExactExpiration_Exists", ApplicationContext.NetworkContext.CurrentDateTime.AddSeconds(2), () => "value2");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("value1");
+            MemoryCache.Default.Get("GetOrAddAndGetExactExpiration_Exists").ShouldHaveSameValueAs("value1");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetOrAddAndGetExactExpiration_NotExists()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("GetOrAddAndGetExactExpiration_NotExists");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.GetOrAddAndGetExactExpiration("GetOrAddAndGetExactExpiration_NotExists", ApplicationContext.NetworkContext.CurrentDateTime.AddSeconds(2), () => "value2");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("value2");
+            MemoryCache.Default.Get("GetOrAddAndGetExactExpiration_NotExists").ShouldHaveSameValueAs("value2");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void GetOrAddAndGetExactExpiration_NotExists_Timeout()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("GetOrAddAndGetExactExpiration_NotExists_Timeout");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            var actual = componentUnderTest.GetOrAddAndGetExactExpiration("GetOrAddAndGetExactExpiration_NotExists_Timeout", ApplicationContext.NetworkContext.CurrentDateTime.AddSeconds(2), () => "value2");
+
+            // Assert
+            actual.ShouldHaveSameValueAs("value2");
+            MemoryCache.Default.Get("GetOrAddAndGetExactExpiration_NotExists_Timeout").ShouldHaveSameValueAs("value2");
+
+            for (var i = 0; i < 3; i++)
+            {
+                Thread.Sleep(500);
+                componentUnderTest.Get<string>("GetOrAddAndGetExactExpiration_NotExists_Timeout").ShouldHaveSameValueAs("value2");
+            }
+            Thread.Sleep(501);
+            componentUnderTest.Get<string>("GetOrAddAndGetExactExpiration_NotExists_Timeout").ShouldHaveSameValueAs(null);
+        }
+
+
+        #endregion
+
+
+        #region Add
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void Add_Exists()
+        {
+            // Arrange
+            MemoryCache.Default.Add("Add_Exists", "value1", new CacheItemPolicy());
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.Add("Add_Exists", "value2");
+
+            // Assert
+            MemoryCache.Default.Get("Add_Exists").ShouldHaveSameValueAs("value2");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void Add_NotExists()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("Add_NotExists");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.Add("Add_NotExists", "value2");
+
+            // Assert
+            MemoryCache.Default.Get("Add_NotExists").ShouldHaveSameValueAs("value2");
+        }
+
+
+        #endregion
+
+
+        #region AddSlidingExpiration
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void AddSlidingExpiration_Exists()
+        {
+            // Arrange
+            MemoryCache.Default.Add("AddSlidingExpiration_Exists", "value1", new CacheItemPolicy());
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.AddSlidingExpiration("AddSlidingExpiration_Exists", "value2", TimeSpan.FromMinutes(1));
+
+            // Assert
+            MemoryCache.Default.Get("AddSlidingExpiration_Exists").ShouldHaveSameValueAs("value2");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void AddSlidingExpiration_NotExists()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("AddSlidingExpiration_NotExists");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.AddSlidingExpiration("AddSlidingExpiration_NotExists", "value2", TimeSpan.FromMinutes(1));
+
+            // Assert
+            MemoryCache.Default.Get("AddSlidingExpiration_NotExists").ShouldHaveSameValueAs("value2");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void AddSlidingExpiration_NotExists_RenewAndThenTimeout()
+        {
+            // Arrange
+            const string key = "AddSlidingExpiration_NotExists_RenewAndThenTimeout";
+            MemoryCache.Default.Remove(key);
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.AddSlidingExpiration(key, "value2", TimeSpan.FromSeconds(2));
+
+            // Assert
+            componentUnderTest.Get<string>(key).ShouldHaveSameValueAs("value2");
+
+            for (var i = 0; i < 5; i++)
+            {
+                Thread.Sleep(500);
+                componentUnderTest.Get<string>(key).ShouldHaveSameValueAs("value2");
+            }
+            Thread.Sleep(2100);
+            componentUnderTest.Get<string>(key).ShouldHaveSameValueAs(null);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void AddSlidingExpiration_NotExists_Timeout()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("AddSlidingExpiration_NotExists_Timeout");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.AddSlidingExpiration("AddSlidingExpiration_NotExists_Timeout", "value2", TimeSpan.FromSeconds(2));
+
+            // Assert
+            MemoryCache.Default.Get("AddSlidingExpiration_NotExists_Timeout").ShouldHaveSameValueAs("value2");
+
+            Thread.Sleep(2100);
+            componentUnderTest.Get<string>("AddSlidingExpiration_NotExists_Timeout").ShouldHaveSameValueAs(null);
+        }
+
+
+        #endregion
+
+
+        #region AddExactExpiration
+
+
+        [TestMethod, TestCategory("Unit")]
+        public void AddExactExpiration_Exists()
+        {
+            // Arrange
+            MemoryCache.Default.Add("AddExactExpiration_Exists", "value1", new CacheItemPolicy());
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.AddExactExpiration("AddExactExpiration_Exists", "value2", ApplicationContext.NetworkContext.CurrentDateTime.AddSeconds(2));
+
+            // Assert
+            MemoryCache.Default.Get("AddExactExpiration_Exists").ShouldHaveSameValueAs("value2");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void AddExactExpiration_NotExists()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("AddExactExpiration_NotExists");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.AddExactExpiration("AddExactExpiration_NotExists", "value2", ApplicationContext.NetworkContext.CurrentDateTime.AddSeconds(2));
+
+            // Assert
+            MemoryCache.Default.Get("AddExactExpiration_NotExists").ShouldHaveSameValueAs("value2");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void AddExactExpiration_NotExists_Timeout()
+        {
+            // Arrange
+            MemoryCache.Default.Remove("AddExactExpiration_NotExists_Timeout");
+            var componentUnderTest = new MemoryCacheAdapter();
+
+            // Act
+            componentUnderTest.AddExactExpiration("AddExactExpiration_NotExists_Timeout", "value2", ApplicationContext.NetworkContext.CurrentDateTime.AddSeconds(2));
+
+            // Assert
+            MemoryCache.Default.Get("AddExactExpiration_NotExists_Timeout").ShouldHaveSameValueAs("value2");
+
+            for (var i = 0; i < 3; i++)
+            {
+                Thread.Sleep(500);
+                componentUnderTest.Get<string>("AddExactExpiration_NotExists_Timeout").ShouldHaveSameValueAs("value2");
+            }
+            Thread.Sleep(500);
+            componentUnderTest.Get<string>("AddExactExpiration_NotExists_Timeout").ShouldHaveSameValueAs(null);
+        }
+
+
+        #endregion
+    }
+}
--- Core.Runtime.Caching/Runtime/Caching/MemoryCacheAdapter.cs
+++ Core.Runtime.Caching/Runtime/Caching/MemoryCacheAdapter.cs
@@ -1,24 +1,33 @@
 ﻿using System;
+using System.Runtime.Caching;
 
 namespace Sfa.Core.Runtime.Caching
 {
     /// <summary>
-    /// Defines the interface for a generic cache.
+    /// An <see cref="ICache"/> implementation of <see cref="MemoryCache"/>.
     /// </summary>
-    public interface ICache
+    public class MemoryCacheAdapter : ICache
     {
+        #region ICache Implementation
+
         /// <summary>
         /// Returns a flag indicating whether or not the cache contains an item for the given <paramref name="key"/>.
         /// </summary>
         /// <param name="key">The key to look for in the cache.</param>
         /// <returns><c>true</c> if the cache contains the item for the key; otherwise, <c>false</c>.</returns>
-        bool ContainsKey(string key);
+        public bool ContainsKey(string key)
+        {
+            return MemoryCache.Default.Contains(key);
+        }
 
         /// <summary>
         /// Deletes the item in the cache with the given key.
         /// </summary>
         /// <param name="key">The key to delete from the cache.</param>
-        void Delete(string key);
+        public void Delete(string key)
+        {
+            MemoryCache.Default.Remove(key);
+        }
 
         /// <summary>
         /// Gets the item for the given <paramref name="key"/>.
@@ -26,7 +35,10 @@ namespace Sfa.Core.Runtime.Caching
         /// <typeparam name="T">The <see cref="Type"/> expected in the cache for the given key.</typeparam>
         /// <param name="key">The key that identifies the item stored in the cache.</param>
         /// <returns>The item stored under the given <see cref="key"/>.</returns>
-        T Get<T>(string key);
+        public T Get<T>(string key)
+        {
+            return (T) MemoryCache.Default.Get(key);
+        }
 
         /// <summary>
         /// Gets the item for the given <paramref name="key"/>. If the cache doesn't contain the item, then the function is called that gets the item and then it is added to the cache.
@@ -35,7 +47,10 @@ namespace Sfa.Core.Runtime.Caching
         /// <param name="key">The key that identifies the item stored in the cache.</param>
         /// <param name="get">The function that provides the value if one isn't found in the cache.</param>
         /// <returns>The item stored under the given <see cref="key"/>.</returns>
-        T GetOrAddAndGet<T>(string key, Func<T> get);
+        public T GetOrAddAndGet<T>(string key, Func<T> get)
+        {
+            return GetOrAddOrReplace(key, new CacheItemPolicy(), get);
+        }
 
         /// <summary>
         /// Gets the item for the given <paramref name="key"/>. If the cache doesn't contain the item, then the function is called that gets the item and then it is added to the cache.
@@ -46,7 +61,10 @@ namespace Sfa.Core.Runtime.Caching
         /// <param name="slidingExpiration">The sliding time to store the item in the cache.</param>
         /// <param name="get">The function that provides the value if one isn't found in the cache.</param>
         /// <returns>The item stored under the given <see cref="key"/>.</returns>
-        T GetOrAddAndGetSlidingExpiration<T>(string key, TimeSpan slidingExpiration, Func<T> get);
+        public T GetOrAddAndGetSlidingExpiration<T>(string key, TimeSpan slidingExpiration, Func<T> get)
+        {
+            return GetOrAddOrReplace(key, new CacheItemPolicy { SlidingExpiration = slidingExpiration }, get);
+        }
 
         /// <summary>
         /// Gets the item for the given <paramref name="key"/>. If the cache doesn't contain the item, then the function is called that gets the item and then it is added to the cache.
@@ -57,7 +75,10 @@ namespace Sfa.Core.Runtime.Caching
         /// <param name="absoluteExpiration">The time at which the item should be evicted from the cache.</param>
         /// <param name="get">The function that provides the value if one isn't found in the cache.</param>
         /// <returns>The item stored under the given <see cref="key"/>.</returns>
-        T GetOrAddAndGetExactExpiration<T>(string key, DateTime absoluteExpiration, Func<T> get);
+        public T GetOrAddAndGetExactExpiration<T>(string key, DateTime absoluteExpiration, Func<T> get)
+        {
+            return GetOrAddOrReplace(key, new CacheItemPolicy { AbsoluteExpiration = new DateTimeOffset(absoluteExpiration) }, get);
+        }
 
         /// <summary>
         /// Adds the given item to the cache under the given key.
@@ -65,7 +86,10 @@ namespace Sfa.Core.Runtime.Caching
         /// <typeparam name="T">The <see cref="Type"/> of the item to be stored in the cache.</typeparam>
         /// <param name="key">The key to store the item under in the cache.</param>
         /// <param name="value">The value to store in the cache.</param>
-        void Add<T>(string key, T value);
+        public void Add<T>(string key, T value)
+        {
+            AddOrReplace(key, value, new CacheItemPolicy());
+        }
 
         /// <summary>
         /// Adds the given item to the cache under the given key with a sliding expiration.
@@ -74,7 +98,15 @@ namespace Sfa.Core.Runtime.Caching
         /// <param name="key">The key to store the item under in the cache.</param>
         /// <param name="value">The value to store in the cache.</param>
         /// <param name="slidingExpiration">The time to store the item in the cache. Each time the item is accessed this time is reset. If the time is passed without the item being accessed, then item is cleared from the cache.</param>
-        void AddSlidingExpiration<T>(string key, T value, TimeSpan slidingExpiration);
+        public void AddSlidingExpiration<T>(string key, T value, TimeSpan slidingExpiration)
+        {
+            AddOrReplace(key, value, new CacheItemPolicy { SlidingExpiration = slidingExpiration, RemovedCallback =
+                arguments =>
+                {
+                    Console.WriteLine("removed");
+                }
+            });
+        }
 
         /// <summary>
         /// Adds the given item to the cache under the given key with an exact expiration time.
@@ -83,6 +115,42 @@ namespace Sfa.Core.Runtime.Caching
         /// <param name="key">The key to store the item under in the cache.</param>
         /// <param name="value">The value to store in the cache.</param>
         /// <param name="absoluteExpiration">The time at which the item should be evicted from the cache.</param>
-        void AddExactExpiration<T>(string key, T value, DateTime absoluteExpiration);
+        public void AddExactExpiration<T>(string key, T value, DateTime absoluteExpiration)
+        {
+            AddOrReplace(key, value, new CacheItemPolicy { AbsoluteExpiration = new DateTimeOffset(absoluteExpiration) });
+        }
+
+        #endregion
+
+
+        #region Helpers
+        private T GetOrAddOrReplace<T>(string key, CacheItemPolicy policy, Func<T> get)
+        {
+            var cache = MemoryCache.Default;
+            var value = (T)cache.Get(key);
+
+            if (value == null)
+            {
+                value = get();
+                AddOrReplace(key, value, policy);
+            }
+
+            return value;
+        }
+
+        private void AddOrReplace<T>(string key, T value, CacheItemPolicy policy)
+        {
+            var cache = MemoryCache.Default;
+
+            var existing = cache.AddOrGetExisting(key, value, policy);
+
+            if (existing != null)
+            {
+                cache.Remove(key);
+                cache.Add(key, value, policy);
+            }
+        }
+
+        #endregion
     }
 }
